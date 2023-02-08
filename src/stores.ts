42535c4b471e55cc38ef05b6c8f12b58ed89901a
Brian Ginsburg <gins@brianginsburg.com>
2023-02-08 14:53:18 -0800

    Add local-only filesystem

--- src/stores.ts
+++ src/stores.ts
@@ -1,8 +1,12 @@
 import { writable } from 'svelte/store'
 import type { Writable } from 'svelte/store'
 
+
+import type { FileSystem } from 'webnative'
 import { version } from '../package.json'
 import type { Patch } from '$lib/patch'
+
+export const localOnlyFsStore: Writable<FileSystem | null> = writable(null)
 
 export const patchStore: Writable<Patch> = writable({
   version,
