3fb80c7f742be391a136f0c76dc8e68bda42ebef
avivash <av@andrewvivash.com>
2023-02-14 11:24:09 -0800

    Feat: add notification store(unstyled)

--- src/components/Presets/PresetHeader.svelte
+++ src/components/Presets/PresetHeader.svelte
@@ -2,17 +2,21 @@
   import { createEventDispatcher } from 'svelte'
 
   import { patchStore } from '../../stores'
+  import { addNotification } from '$lib/notifications'
   import ArrowLeft from '$components/icons/ArrowLeft.svelte'
   import ArrowRight from '$components/icons/ArrowRight.svelte'
+  import Notifications from '$components/notifications/Notifications.svelte'
 
   const dispatch = createEventDispatcher()
 
   const handleClick = () => {
-    dispatch('click', { view: 'presets' })
+    // dispatch('click', { view: 'presets' })
+    addNotification('A notification', 'success')
   }
 </script>
 
 <div class="relative flex flex-col gap-2 min-w-[300px] items-center justify-center p-2 rounded border-2 border-secondary">
+  <Notifications />
   <h3 class="text-lg"><button on:click={handleClick}>{$patchStore.name}</button></h3>
 
   <div class="flex w-full mt-auto justify-between">
--- src/stores.ts
+++ src/stores.ts
@@ -1,11 +1,14 @@
 import { writable, type Writable } from 'svelte/store'
 import type { FileSystem } from 'webnative'
 
+import type { Notification } from '$lib/notifications'
 import { DEFAULT_PATCH, type Patch } from '$lib/patch'
 import { DEFAULT_CATEGORIES } from '$lib/presets/constants'
 import type { Presets } from '$lib/presets'
 
 export const localOnlyFsStore: Writable<FileSystem | null> = writable(null)
+
+export const notificationStore: Writable<Notification[]> = writable([])
 
 export const patchStore: Writable<Patch> = writable(DEFAULT_PATCH)
 
