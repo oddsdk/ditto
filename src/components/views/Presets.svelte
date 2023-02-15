d7da2d8121b09db8e3e73ddddbf7fc847d9ac66e
avivash <av@andrewvivash.com>
2023-02-14 17:08:19 -0800

    Feat: add basic search functionality

--- src/components/views/Presets.svelte
+++ src/components/views/Presets.svelte
@@ -5,6 +5,7 @@
   import EditPreset from '$components/presets/EditPreset.svelte'
   import PresetInfo from '$components/presets/PresetInfo.svelte'
 
+  let isSearching = false
   let adding = false
   let editing = false
   let presets = $presetsStore.presets
@@ -50,6 +51,13 @@
     editing = false
   }
 
+  // Filter presets based on a search parameter
+  const handlePresetSearch = (event: Event): void => {
+    const { value } = event.target as HTMLInputElement
+    console.log('value', value)
+    presets = $presetsStore.presets.filter(({ name }) => name.toLowerCase().indexOf(value.toLowerCase()) !== -1)
+  }
+
   // Handle "Add Preset" button click
   const handleAddPresetClick = () => adding = true
 
@@ -62,11 +70,13 @@
   <h4 class="col-span-1 pl-10 py-3 text-lg flex items-center justify-between">Presets <button on:click={handleAddPresetClick} class="btn btn-secondary btn-xs">Add Preset</button></h4>
   <h4 class="col-span-1 pl-7 py-3 text-lg">Preset Info</h4>
 </div>
+
 <div class="grid-container grid grid-cols-3 divide-x-2 min-h-[calc(100vh-220px)] gap-4">
 
   <div class="col-span-1 px-4">
     <div class="w-full">
-      <input type="text" placeholder="Search presets" class="w-full text-sm bg-base-100 border-2 border-neutral rounded h-10 py-3 px-4 mb-2 focus:outline-none" />
+      <input on:input={handlePresetSearch} type="text" placeholder="Search presets" spellcheck="false" name="search" class="w-full text-sm bg-base-100 border-2 transition-colors rounded h-10 py-3 px-4 mb-2 focus:border-secondary focus:outline-none" />
+
       {#each $presetsStore.categories as category}
         <button on:click={() => handleCategoryClick(category)} class="w-full flex cursor-pointer py-2 px-4 hover:bg-primary hover:text-white transition-colors ease-in-out rounded capitalize text-sm {$presetsStore.selectedCategory === category ? 'bg-primary text-white' : ''}">
           <h5>{category}</h5>
