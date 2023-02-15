e06b6a2f3b6fbc1adcd0413d474db09ea7e26bf9
avivash <av@andrewvivash.com>
2023-02-15 10:31:57 -0800

    Feat: add hover effect

--- src/components/icons/Delete.svelte
+++ src/components/icons/Delete.svelte
@@ -23,5 +23,11 @@
 </script>
 
 <button disabled={loading} on:click={handleDeleteClick}>
-  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" class="feather feather-trash-2" viewBox="0 0 24 24"><path d="M3 6h18M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2M10 11v6M14 11v6"/></svg>
+  <svg class="feather feather-trash-2 delete transition transition-all ease-in-out" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24"><path d="M3 6h18M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2M10 11v6M14 11v6"/></svg>
 </button>
+
+<style>
+  .delete:hover {
+    fill: currentColor;
+  }
+</style>
