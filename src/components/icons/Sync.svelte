888cd7a42105b2fc82f9887f5d1d3e87ad174dce
avivash <av@andrewvivash.com>
2023-02-17 11:24:36 -0800

    Chore: add title to sync button

--- src/components/icons/Sync.svelte
+++ src/components/icons/Sync.svelte
@@ -14,5 +14,5 @@
 </script>
 
 <button on:click={handleSyncClick}>
-  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" class="feather feather-refresh-cw" viewBox="0 0 24 24"><path d="M23 4v6h-6M1 20v-6h6"/><path d="M3.51 9a9 9 0 0 1 14.85-3.36L23 10M1 14l4.64 4.36A9 9 0 0 0 20.49 15"/></svg>
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" class="feather feather-refresh-cw" viewBox="0 0 24 24"><title>Sync</title><path d="M23 4v6h-6M1 20v-6h6"/><path d="M3.51 9a9 9 0 0 1 14.85-3.36L23 10M1 14l4.64 4.36A9 9 0 0 0 20.49 15"/></svg>
 </button>
